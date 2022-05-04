90e55594a760b20b367bea72fd7352d3c7a9bb0f
jjcapellan <soycape@hotmail.com>
2022-05-04 22:28:31 +0200

    Code formatting

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -11,103 +11,103 @@ export default class Menu extends Phaser.Scene {
 
         // Top score
         let topScore = getCookie(COOKIE_TOP).padStart(6, '0');
-        this.add.text(324, 1012, `TOP-${topScore}`,{ fontSize: 24 }).setOrigin(0.5);
+        this.add.text(324, 1012, `TOP-${topScore}`, { fontSize: 24 }).setOrigin(0.5);
 
         // Board
         this.add.image(0, 0, 'atlas', 'table')
-          .setOrigin(0);
+            .setOrigin(0);
 
         // Play sound
         this.snd_play = this.sound.add('go');
 
         // Menu contents
         this.createContents();
-    
+
         const configCamera = {
-          x: 121,
-          y: 195,
-          width: 406,
-          height: 770,
-          contentBounds: {
-            x: 2000,
-            y: 2000,
-            length: 3 * 406
-          },
-          drag: 0.95,
-          snap: {
-            enable: true,
-            padding: 406
-          },
-          horizontal: true
+            x: 121,
+            y: 195,
+            width: 406,
+            height: 770,
+            contentBounds: {
+                x: 2000,
+                y: 2000,
+                length: 3 * 406
+            },
+            drag: 0.95,
+            snap: {
+                enable: true,
+                padding: 406
+            },
+            horizontal: true
         }
-    
+
         this.menuCamera = new ScrollingCamera(this, configCamera);
-      }
-    
-      createContents() {
+    }
+
+    createContents() {
         const originX = 2000;
         const originY = 2000;
         const snapPadding = 406;
-    
+
         this.createMenu(originX, originY);
         this.createCredits(originX + 1 * snapPadding, originY);
         this.createControls(originX + 2 * snapPadding, originY);
-      }
-    
-      createMenu(originX, originY) {
+    }
+
+    createMenu(originX, originY) {
         const centerX = originX + 406 / 2;
         const originYbt = originY + 300;
-    
+
         this.add.image(centerX, originY + 140, 'atlas-menu', 'logoblack');
-    
+
         // Button play
         this.bt_play = this.add.image(centerX, originYbt, 'atlas-menu', 'btplay')
-          .setInteractive();
+            .setInteractive();
         this.bt_play.on('pointerdown', () => {
             this.snd_play.play();
-          this.scene.start('inGame');
+            this.scene.start('inGame');
         });
-    
+
         // Button controls
         this.bt_controls = this.add.image(centerX, originYbt + 1 * 130, 'atlas-menu', 'btcontrols')
-          .setInteractive();
+            .setInteractive();
         this.bt_controls.on('pointerdown', () => {
-          this.menuCamera.setSpeed(2 * 1300);
+            this.menuCamera.setSpeed(2 * 1300);
         });
-    
+
         //Button credits
         this.bt_credits = this.add.image(centerX, originYbt + 2 * 130, 'atlas-menu', 'btcredits')
-          .setInteractive();
+            .setInteractive();
         this.bt_credits.on('pointerdown', () => {
-          this.menuCamera.setSpeed(1235);
+            this.menuCamera.setSpeed(1235);
         });
-    
-      }
-    
-      createCredits(originX, originY) {
+
+    }
+
+    createCredits(originX, originY) {
         const centerX = originX + 406 / 2;
-    
+
         this.add.image(centerX, originY + 100, 'atlas-menu', 'credits').setOrigin(0.5, 0);
-    
+
         this.bt_back_credits = this.add.image(centerX, originY + 770 - 100, 'atlas-menu', 'btback')
-          .setInteractive();
-    
+            .setInteractive();
+
         this.bt_back_credits.on('pointerdown', () => {
-          this.menuCamera.setSpeed(-2000);
+            this.menuCamera.setSpeed(-2000);
         });
-    
-      }
-    
-      createControls(originX, originY) {
+
+    }
+
+    createControls(originX, originY) {
         const centerX = originX + 406 / 2;
-    
+
         this.add.image(centerX, originY + 100, 'atlas-menu', 'controls').setOrigin(0.5, 0);
-    
+
         this.bt_back_controls = this.add.image(centerX, originY + 770 - 100, 'atlas-menu', 'btback')
-          .setInteractive();
-    
+            .setInteractive();
+
         this.bt_back_controls.on('pointerdown', () => {
-          this.menuCamera.setSpeed(-2 * 2000);
+            this.menuCamera.setSpeed(-2 * 2000);
         });
-      }
+    }
 }
